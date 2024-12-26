fac7896b62e383f705b84476a72bc0161123b99c
Seo <seo@dayoff.co.kr>
2024-12-26 15:44:03 +0900

    Add main layout

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import BingoBoard from "./components/BingoBoard";
+import Title from "./components/Title";
+
+function App() {
+  return (
+    <main className="w-screen h-screen overflow-hidden font-pixelify-sans px-3 pb-[60px] md:py-5 md:px-0 bg-bingo-darkBlue">
+      <div
+        className={"h-full flex flex-col w-full md:max-w-screen-md md:mx-auto"}
+      >
+        <Title />
+        <BingoBoard />
+      </div>
+    </main>
+  );
+}
+
+export default App;
