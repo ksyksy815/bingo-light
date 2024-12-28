25209016ecebe0e10cf60cc366df1f8c0d5bf917
Seoyeon Kim <seoyeonkim815@gmail.com>
2024-12-28 10:22:56 +0900

    Add a game start button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import NewBingoBoard from "./components/NewBingoBoard";
+import { useState } from "react";
+import BingoBoard from "./components/BingoBoard";
 import Title from "./components/Title";
+import GameStartButton from "./components/GameStartButton";
 
 function App() {
+  const [startGame, setStartGame] = useState(false);
+
+  const handleClick = () => setStartGame(true);
+
   return (
-    <main className="w-screen h-[100svh] overflow-hidden font-pixelify-sans px-3 pb-[60px] md:py-5 md:px-0 bg-bingo-darkBlue">
+    <main className="flex flex-col w-screen h-[100svh] overflow-hidden font-pixelify-sans px-3 pb-[60px] md:py-5 md:px-0 bg-bingo-darkBlue">
       <div
         className={"h-full flex flex-col w-full md:max-w-screen-md md:mx-auto"}
       >
         <Title />
-        <NewBingoBoard />
+        {
+          startGame ? <BingoBoard /> : <GameStartButton handleClick={handleClick} />
+        }
+        
       </div>
     </main>
   );
